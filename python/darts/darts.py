6d5fc9bbbcd64b9fc6e51ccf3e716db5a7575f82
Gergely Imreh <imrehg@gmail.com>
2019-08-21 23:45:38 +0100

    python/darts
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

---
+++ python/darts/darts.py
@@ -0,0 +1,30 @@
+"""Calculating darts scores 🎯"""
+
+import math
+
+"""Darts circle radii and score settings"""
+DARTS_RADIUS_SCORE = [{'radius': 1, 'score': 10},
+                      {'radius': 5, 'score': 5},
+                      {'radius': 10, 'score': 1}]
+
+def score(x, y):
+    """
+    Calculate darts score based on hit location
+
+    Args:
+        x, y: hit location carthesian coordinates
+
+    Constants:
+        DARTS_RADIUS_SCORE: storing radii and
+            corresponding scores
+
+    Return:
+        Score relevant to the circle being hit
+    """
+    distance = math.sqrt(x*x + y*y)
+    thrower_score = 0
+    for circle in DARTS_RADIUS_SCORE:
+        if distance <= circle['radius']:
+            thrower_score = circle['score']
+            break
+    return thrower_score
