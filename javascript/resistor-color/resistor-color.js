7e0578adf087f9b25de308a865775c588f82e6a8
Gergely Imreh <imrehg@gmail.com>
2019-08-21 21:54:28 +0100

    javascript/resistpr-color
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,28 @@
+/**
+ * Find resistor value by color code, as per
+ * https://en.wikipedia.org/wiki/Electronic_color_code
+ * @param {string} color - The color to look up (lowercase).
+ * @return {integer} The corresponding resistance value.
+ * @throws Will throw an error if color code is unknown.
+ */
+export const colorCode = color => {
+  let resistorValue = COLORS.indexOf(color);
+  if (resistorValue < 0) {
+    throw new Error("Unknown resistor color.");
+  }
+  return resistorValue;
+};
+
+/** The list of colors in order of increasing value */
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
