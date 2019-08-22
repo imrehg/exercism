75b716eb54768eda9df7cb6ad8ee94213481cd1f
Gergely Imreh <imrehg@gmail.com>
2019-08-22 18:48:12 +0100

    Submission rewrite of bash/reverse-string
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -4,10 +4,10 @@ main () {
     local inString=$1
     local outString=""
     local letter
-    while [ "${#inString}" -gt 0 ]; do
-        letter=${inString:${#inString}-1:1}
+    while (( ${#inString} > 0 )); do
+        letter="${inString:${#inString}-1:1}"
         outString="${outString}${letter}"
-        inString=${inString%${letter}}
+        inString="${inString%${letter}}"
     done
     echo "$outString"
 }
