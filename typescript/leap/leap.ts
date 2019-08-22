2f3ba7ace066238deddd3d8c31ae56c2ca5882cd
Gergely Imreh <imrehg@gmail.com>
2019-08-22 18:45:14 +0100

    typescript/leap submission
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,13 @@
+/**
+ * Claculate if a year is a leap year.
+ *
+ * @param year - The year to check
+ * @returns Whether or not the year is a leap
+ */
+function isLeapYear(year: number): boolean {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    ? true
+    : false;
+}
+
+export default isLeapYear;
