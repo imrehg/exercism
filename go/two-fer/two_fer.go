ab3d7870b66db67a19e7badcf7d02719660dec44
Gergely Imreh <imrehg@gmail.com>
2019-08-26 12:20:49 +0100

    Update go/two-fer
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,7 +1,9 @@
+// Package twofer provides a nice way of sharing.
 package twofer
 
 import "fmt"
 
+// ShareWith provides you with a handy feedback on how to share.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
