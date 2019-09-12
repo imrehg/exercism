fc729de40dd706d9abd6736230806fb5a9b63941
Gergely Imreh <imrehg@gmail.com>
2019-09-12 23:05:27 +0100

    Update rust/bob (likely still not acceptable for the mentor)
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
-use regex::Regex;
+use regex::RegexSet;
 
 /// Chatting with Bob
 ///
@@ -37,24 +37,23 @@ use regex::Regex;
 /// ```
 pub fn reply(message: &str) -> &str {
     lazy_static! {
-        // Matching when we are yelled at (at least one capital letter, and the rest is not lowercase)
-        static ref YELL: Regex = Regex::new(r"^\s*[^a-z]*[A-Z]+[^a-z]*\s*$").unwrap();
-        // Mathing a question (any word caracters and last punctuation is a question mark)
-        static ref QUESTION: Regex = Regex::new(r"^.*\w?.*\?\s*$").unwrap();
+        static ref SET: RegexSet = RegexSet::new(&[
         // Matching a question that was yelled at us (there are some letters and all are capitals, finished witn a question mark)
-        static ref YELL_QUESTION: Regex = Regex::new(r"^\s*[^a-z]*[A-Z]+[^a-z]*\?\s*$").unwrap();
+        r"^\s*[^a-z]*[A-Z]+[^a-z]*\?\s*$",
+        // Matching when we are yelled at (at least one capital letter, and the rest is not lowercase)
+        r"^\s*[^a-z]*[A-Z]+[^a-z]*\s*$",
+        // Mathing a question (any word caracters and last punctuation is a question mark)
+        r"^.*\w?.*\?\s*$",
         // Matching when there's nothing being said
-        static ref NOTHING: Regex = Regex::new(r"^\s*$").unwrap();
+        r"^\s*$",
+        ]).unwrap();
     }
-    if YELL_QUESTION.is_match(message) {
-        return "Calm down, I know what I'm doing!";
-    } else if QUESTION.is_match(message) {
-        return "Sure.";
-    } else if NOTHING.is_match(message) {
-        return "Fine. Be that way!";
-    } else if YELL.is_match(message) {
-        return "Whoa, chill out!";
-    } else {
-        return "Whatever.";
+    let _patterns: Vec<_> = SET.matches(message).into_iter().collect();
+    match _patterns.first() {
+        Some(0) => { "Calm down, I know what I'm doing!" }
+        Some(1) => { "Whoa, chill out!" }
+        Some(2) => { "Sure." }
+        Some(3) => { "Fine. Be that way!" }
+        _ => { "Whatever." }
     }
 }
