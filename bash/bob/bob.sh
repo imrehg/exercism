d9e21b3e9e0f572294a4eecb9ee73526756821e0
Gergely Imreh <imrehg@gmail.com>
2020-07-04 16:58:38 +0100

    bash/bob: simplify
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -2,13 +2,20 @@
 
 main () {
     local message=$1
-    if [[ $message =~ ^[[:space:]]*[^[:lower:]]*[[:upper:]][^[:lower:]]*\?[[:space:]]*$ ]]; then
+    # Remove leading and trailing spaces from message
+    shopt -s extglob
+    message="${message%%*([[:space:]])}"
+    message="${message##*([[:space:]])}"
+    shopt -u extglob
+    shout_pattern='^[^[:lower:]]*[[:upper:]][^[:lower:]]*'
+
+    if [[ $message =~ $shout_pattern\?$ ]]; then
         echo "Calm down, I know what I'm doing!"
-    elif [[ $message =~ ^[[:space:]]*[^[:lower:]]*[[:upper:]][^[:lower:]]*[[:space:]]*$ ]]; then
+    elif [[ $message =~ $shout_pattern$ ]]; then
         echo "Whoa, chill out!"
-    elif [[ $message =~ ^.*[^[:space:]].*\?[[:space:]]*$ ]]; then
+    elif [[ $message =~ ^.*[^[:space:]].*\?$ ]]; then
         echo "Sure."
-    elif [[ "$message" =~ ^[[:space:]]*$ ]]; then
+    elif [[ "$message" == "" ]]; then
         echo "Fine. Be that way!"
     else
         echo "Whatever."
