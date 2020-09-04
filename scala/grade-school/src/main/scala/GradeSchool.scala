abb7a6708dd8b7ce903e510a6087fc76c70c3199
Gergely Imreh <imrehg@gmail.com>
2020-09-04 23:24:56 +0100

    scala/grade-school: submission
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

---
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -0,0 +1,20 @@
+import scala.collection.mutable.{Map, SortedMap}
+
+class School {
+  type DB = Map[Int, Seq[String]]
+
+  private var _db: DB = Map()
+
+  def add(name: String, g: Int) = {
+    if (_db contains g) _db(g) = _db(g) :+ name
+    else _db += (g -> Seq(name))
+  }
+
+  def db: DB = _db
+
+  def grade(g: Int): Seq[String] =
+    if (_db contains g) _db(g)
+    else Seq()
+
+  def sorted: DB = SortedMap(_db.toSeq: _*) map { case (k, v) => k -> v.sorted }
+}
