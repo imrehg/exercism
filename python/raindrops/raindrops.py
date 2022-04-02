3bce149b486647208a7ee2e41c59371dd8a8741c
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-02 21:30:16 +0800

    python:raindrops submission

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,14 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+    result = ""
+
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+
+    if result == "":  # None of the above are factors
+        result = str(number)
+
+    return result
