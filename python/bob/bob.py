274e48a9873076e8e81b4d820503e3dcdf62ec71
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-04 23:18:43 +0800

    python:bob initial iteration

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,17 @@
-def response(hey_bob):
-    pass
+def response(hey_bob: str) -> str:
+    hey_bob = hey_bob.strip()
+
+    if hey_bob == "":
+        return "Fine. Be that way!"
+
+    is_yelling = hey_bob.isupper()
+    is_question = hey_bob[-1] == "?"
+
+    if is_yelling and is_question:
+        return "Calm down, I know what I'm doing!"
+    if is_yelling:
+        return "Whoa, chill out!"
+    if is_question:
+        return "Sure."
+
+    return "Whatever."
