482afdfafa18708131512a281fc10b19602d2cf1
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-04 22:52:13 +0800

    python:grains submission

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,13 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+    """Number of grains on a chessboard square given its order."""
+    if not (1 <= number <= 64):
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number - 1)
 
 
-def total():
-    pass
+def total() -> int:
+    """The total number of grains on a chessboard according to the doubling rules"""
+    # This sum:
+    # sum([square(n) for n in range(1, 65)])
+    # actually adds up to:
+    return 2**64 - 1
