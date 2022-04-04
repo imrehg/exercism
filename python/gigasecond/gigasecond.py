fdecac3a116bde4140dd625ed0c0d075396ef0d4
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-04 22:35:55 +0800

    python:gigasecond solution

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,6 @@
-def add(moment):
-    pass
+from datetime import datetime, timedelta
+
+
+def add(moment: datetime) -> datetime:
+    """Add a gigasecond to a moment."""
+    return moment + timedelta(seconds=1_000_000_000)
