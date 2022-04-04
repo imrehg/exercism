e6a4f69dcb2fda9f308f30ca36873aeed22038a4
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-04 22:56:49 +0800

    python:leap submission

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,3 @@
-def leap_year(year):
-    pass
+def leap_year(year: int) -> bool:
+    """Is a year a leap year? Let's ask our experts!"""
+    return ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)
