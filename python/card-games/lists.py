ed9a4363274fae89d8e349d6978aee7d8ada1b6c
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-05 08:55:42 +0800

    python:lists resubmission

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -1,21 +1,17 @@
 """Functions for tracking poker hands and assorted card tasks.
-
-Python list documentation: https://docs.python.org/3/tutorial/datastructures.html
 """
 
-from typing import List
 
-
-def get_rounds(number: int) -> List[int]:
+def get_rounds(number: int) -> list[int]:
     """Create a list containing the current and next two round numbers.
 
     :param number: int - current round number.
     :return: list - current round and the two that follow.
     """
-    return [number + round_index for round_index in range(0, 3)]
+    return list(range(number, number + 3))
 
 
-def concatenate_rounds(rounds_1: List[int], rounds_2: List[int]) -> List[int]:
+def concatenate_rounds(rounds_1: list[int], rounds_2: list[int]) -> list[int]:
     """Concatenate two lists of round numbers.
 
     :param rounds_1: list - first rounds played.
@@ -25,7 +21,7 @@ def concatenate_rounds(rounds_1: List[int], rounds_2: List[int]) -> List[int]:
     return rounds_1 + rounds_2
 
 
-def list_contains_round(rounds: List[int], number: int) -> bool:
+def list_contains_round(rounds: list[int], number: int) -> bool:
     """Check if the list of rounds contains the specified number.
 
     :param rounds: list - rounds played.
@@ -35,7 +31,7 @@ def list_contains_round(rounds: List[int], number: int) -> bool:
     return number in rounds
 
 
-def card_average(hand: List[int]) -> float:
+def card_average(hand: list[int]) -> float:
     """Calculate and returns the average card value from the list.
 
     :param hand: list - cards in hand.
@@ -45,7 +41,7 @@ def card_average(hand: List[int]) -> float:
     return sum(hand) / len(hand)
 
 
-def approx_average_is_average(hand: List[int]) -> bool:
+def approx_average_is_average(hand: list[int]) -> bool:
     """Return if an average is using (first + last index values ) OR ('middle' card) == calculated average.
 
     :param hand: list - cards in hand.
@@ -54,10 +50,10 @@ def approx_average_is_average(hand: List[int]) -> bool:
     calculated_average = card_average(hand)
     fast_average_ends = (hand[0] + hand[-1]) / 2
     fast_average_middle = hand[len(hand) // 2]
-    return fast_average_ends == calculated_average or fast_average_middle == calculated_average
+    return calculated_average in {fast_average_ends, fast_average_middle}
 
 
-def average_even_is_average_odd(hand: List[int]) -> bool:
+def average_even_is_average_odd(hand: list[int]) -> bool:
     """Return if the (average of even indexed card values) == (average of odd indexed card values).
 
     :param hand: list - cards in hand.
@@ -66,10 +62,10 @@ def average_even_is_average_odd(hand: List[int]) -> bool:
     return card_average(hand[0::2]) == card_average(hand[1::2])
 
 
-def maybe_double_last(hand: List[int]) -> List[int]:
+def maybe_double_last(hand: list[int]) -> list[int]:
     """Multiply a Jack card value in the last index position by 2.
 
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
     """
-    return hand[0:-1] + [22 if hand[-1] == 11 else hand[-1]]
+    return hand[:-1] + [22 if hand[-1] == 11 else hand[-1]]
