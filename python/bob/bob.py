0a88cb35971c3fca595cd208d1e02132bf4b7251
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-05 09:27:26 +0800

    python:bob update from mentoring

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -5,7 +5,7 @@ def response(hey_bob: str) -> str:
         return "Fine. Be that way!"
 
     is_yelling = hey_bob.isupper()
-    is_question = hey_bob[-1] == "?"
+    is_question = hey_bob.endswith("?")
 
     if is_yelling and is_question:
         return "Calm down, I know what I'm doing!"
