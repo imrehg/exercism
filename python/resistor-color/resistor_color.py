86df8e7fcd9640ae136c47401bbd8a564f2235be
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-07 12:51:12 +0800

    python:resistor-color adjustments

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,19 +1,22 @@
+RESISTOR_COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
 def color_code(color: str) -> int:
     """Find a single color's value."""
-    return colors().index(color)
+    return RESISTOR_COLORS.index(color)
 
 
 def colors() -> list[str]:
     """List all the resistor colors in value order."""
-    return [
-        "black",
-        "brown",
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "violet",
-        "grey",
-        "white",
-    ]
+    return RESISTOR_COLORS
