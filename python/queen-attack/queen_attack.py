7393b486bbde7cdbc9b26f53cbf53285779b8356
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-07 10:44:25 +0800

    python:queen_attack solution

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,6 +1,31 @@
 class Queen:
-    def __init__(self, row, column):
-        pass
+    def __init__(self, row: int, column: int) -> None:
+        if row < 0:
+            raise ValueError("row not positive")
+        if row > 7:
+            raise ValueError("row not on board")
+        if column < 0:
+            raise ValueError("column not positive")
+        if column > 7:
+            raise ValueError("column not on board")
 
-    def can_attack(self, another_queen):
-        pass
+        self.row = row
+        self.column = column
+
+    def can_attack(self, another_queen: "Queen") -> bool:
+        """Check if queens can attack each other.
+
+        Attack is possible if they are on the same row,
+        same column, or along the diagonals.
+        """
+        row_offset = abs(self.row - another_queen.row)
+        column_offset = abs(self.column - another_queen.column)
+        if row_offset == column_offset == 0:
+            raise ValueError(
+                "Invalid queen position: both queens in the same square"
+            )
+        return (
+            row_offset == 0
+            or column_offset == 0
+            or row_offset == column_offset
+        )
