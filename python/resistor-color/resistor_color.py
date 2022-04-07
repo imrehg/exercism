4adf5eb3e00438a198524517785404e4180a93eb
Gergely Imreh <gergely.imreh@faculty.ai>
2022-04-07 10:27:47 +0800

    python:resistor-color submission

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,22 @@
-def color_code(color):
-    pass
+from collections import OrderedDict
+
+COLOR_MAPPING = OrderedDict(
+    black=0,
+    brown=1,
+    red=2,
+    orange=3,
+    yellow=4,
+    green=5,
+    blue=6,
+    violet=7,
+    grey=8,
+    white=9,
+)
 
 
-def colors():
-    pass
+def color_code(color: str) -> int:
+    return COLOR_MAPPING[color]
+
+
+def colors() -> list[str]:
+    return list(COLOR_MAPPING.keys())
