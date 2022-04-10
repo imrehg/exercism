98bec01761d0095c45b61e08336874c53111acfe
Gergely Imreh <gergely@imreh.net>
2022-04-10 13:02:10 +0800

    python/resistor-color-duo: submission

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,25 @@
-def value(colors):
-    pass
+"""Resistor Color Duo
+
+https://exercism.org/tracks/python/exercises/resistor-color-duo
+"""
+
+
+RESISTOR_COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def value(colors: list[str]) -> int:
+    """Calculate resistor value based on the first two color bands"""
+    return int(
+        "".join([str(RESISTOR_COLORS.index(color)) for color in colors[:2]])
+    )
