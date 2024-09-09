748ec5ca986db832e5e6da9ec46d14285f4001b3
Gergely Imreh <gergely@imreh.net>
2024-09-09 11:38:18 +0800

    python/collatz_conjecture: initial submission

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
-def steps(number):
-    pass
+def steps(number: int) -> int:
+    """Count the number of steps to reach 1 using the Collatz Conjecture."""
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    step_count = 0
+    while number != 1:
+        step_count += 1
+        number = (number // 2) if (number % 2) == 0 else (3 * number + 1)
+
+    return step_count
