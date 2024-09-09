c24d2afac3c32a917f44f36b36c449a9beb36971
Gergely Imreh <gergely@imreh.net>
2024-09-09 11:29:24 +0800

    python/armstrong_numbers: first submission

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(number: int) -> bool:
+    """Check if a number is an Armstrong number."""
+    digits = [int(digit) for digit in str(number)]
+    exponent = len(digits)
+    return sum([digit**exponent for digit in digits]) == number
