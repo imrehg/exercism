68a5fbb0aa4be71bc2ae93e171155c047194948d
Gergely Imreh <gergely@imreh.net>
2024-09-10 11:49:16 +0800

    python/pig_lating: submission

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,22 @@
-def translate(text):
-    pass
+import re
+
+# Matching patterns for the various Pig Latin grammar rules
+rule_1 = re.compile(r"^([aeiou]|yt|xr)\w*$")
+rule_2_3 = re.compile(r"^(?P<start>[^aeiou]?qu|[^aeiou]+)(?P<rest>\w*)$")
+rule_4 = re.compile(r"^(?P<start>[^aeiou]+)(?P<rest>y\w*)$")
+
+
+def translate(text: str) -> str:
+    """Translate a text to Pig Latin. Tnslatetray aay exttay otay Igpay Atinlay."""
+
+    words = re.split(r"\s+", text)
+    for i, word in enumerate(words):
+        # Apply the various
+        word, sub_count = rule_1.subn(r"\g<0>ay", word)
+        if not sub_count:
+            word, sub_count = rule_4.subn(r"\g<rest>\g<start>ay", word)
+        if not sub_count:
+            word, sub_count = rule_2_3.subn(r"\g<rest>\g<start>ay", word)
+        words[i] = word
+
+    return " ".join(words)
