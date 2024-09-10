d3dca9ba443102e5d904631b2866a3b5f4e4bf43
Gergely Imreh <gergely@imreh.net>
2024-09-10 11:55:34 +0800

    python/pig_latin: revisied

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,9 +1,12 @@
 import re
 
-# Matching patterns for the various Pig Latin grammar rules
+# Matching patterns for the various Pig Latin grammar rules and their transformations
 rule_1 = re.compile(r"^([aeiou]|yt|xr)\w*$")
+rule_1_sub = r"\g<0>ay"
+
 rule_2_3 = re.compile(r"^(?P<start>[^aeiou]?qu|[^aeiou]+)(?P<rest>\w*)$")
 rule_4 = re.compile(r"^(?P<start>[^aeiou]+)(?P<rest>y\w*)$")
+rule_2_3_4_sub = r"\g<rest>\g<start>ay"
 
 
 def translate(text: str) -> str:
@@ -11,12 +14,14 @@ def translate(text: str) -> str:
 
     words = re.split(r"\s+", text)
     for i, word in enumerate(words):
-        # Apply the various
-        word, sub_count = rule_1.subn(r"\g<0>ay", word)
-        if not sub_count:
-            word, sub_count = rule_4.subn(r"\g<rest>\g<start>ay", word)
-        if not sub_count:
-            word, sub_count = rule_2_3.subn(r"\g<rest>\g<start>ay", word)
+        for rule, transform in (
+            (rule_1, rule_1_sub),
+            (rule_4, rule_2_3_4_sub),
+            (rule_2_3, rule_2_3_4_sub),
+        ):
+            word, subs_done = rule.subn(transform, word)
+            if subs_done:
+                break
         words[i] = word
 
     return " ".join(words)
