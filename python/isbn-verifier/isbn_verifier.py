7dc4e52270fa2adfd94d2d4aeb49ee738f39b51a
Gergely Imreh <gergely@imreh.net>
2024-09-10 11:10:26 +0800

    python/isbn: initial submission

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,21 @@
-def is_valid(isbn):
-    pass
+import re
+
+isbn_allowed_format = re.compile(r"^\d{9}[\dX]$")
+
+
+def is_valid(isbn: str) -> bool:
+    """Test a validity of an ISBN-10 number.
+
+    >>> is_valid("3-598-21507-X")
+    True
+
+    >>> is_valid("3-598-21515-X"
+    False
+    """
+    isbn = isbn.replace("-", "")
+    if isbn_allowed_format.match(isbn):
+        digits = [int(d) if d != "X" else 10 for d in isbn]
+        result = sum(d * (10 - i) for i, d in enumerate(digits)) % 11 == 0
+    else:  # invalid format
+        result = False
+    return result
