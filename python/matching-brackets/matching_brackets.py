5e61a24b330c587f50d38f85b7ba510441717230
Gergely Imreh <gergely@imreh.net>
2024-09-14 10:34:42 +0800

    matching-brackets initial submission

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,13 @@
-def is_paired(input_string):
-    pass
+CLOSE_BRACKET_PAIRS = {"}": "{", "]": "[", ")": "("}
+
+
+def is_paired(input_string: str) -> bool:
+    """Matching them brackets."""
+    stack: list[str] = []
+    for character in input_string:
+        if character in CLOSE_BRACKET_PAIRS.values():
+            stack.append(character)
+        elif character in CLOSE_BRACKET_PAIRS.keys():
+            if not stack or stack.pop() != CLOSE_BRACKET_PAIRS[character]:
+                return False
+    return not stack
