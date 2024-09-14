8c6e28914cb8c89eb702332b6d72752d2f66252a
Gergely Imreh <gergely@imreh.net>
2024-09-14 11:29:39 +0800

    python/isogram solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,19 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:
+    """Return whether a string is an isogram.
+
+    An isogram is a word or phrase without a repeating letter.
+
+    >>> is_isogram("lumberjacks")
+    True
+
+    >>> is_isogram("isograms")
+    False
+    """
+    found_letters: set[str] = set()
+    for letter in string.lower():
+        if not letter.isalpha():
+            continue
+        if letter in found_letters:
+            return False
+        found_letters.add(letter)
+    return True
