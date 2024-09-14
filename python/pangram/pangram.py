379f845bd94ebd0b04de046c05a08cd26660fba5
Gergely Imreh <gergely@imreh.net>
2024-09-14 11:49:34 +0800

    submission

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,10 @@
-def is_pangram(sentence):
-    pass
+def is_pangram(sentence: str) -> bool:
+    """Return True if the input sentence is a pangram.
+
+    Pangram is a sentence that uses all 26 letters of the English alphabet.
+
+    >>> is_pangram("abcdefghijklmnopqrstuvwxyz")
+    True
+    """
+    sanitized = [char.lower() for char in sentence if char.isalpha()]
+    return len(set(sanitized)) == 26
