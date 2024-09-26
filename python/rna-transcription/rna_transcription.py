5c1f00823553178b8d381bd14a642778f0b71af6
Gergely Imreh <gergely@imreh.net>
2024-09-26 10:45:41 +0800

    rna

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,3 @@
-def to_rna(dna_strand):
-    pass
+def to_rna(dna_strand: str) -> str:
+    # This relies on the input being all upper case, and the expected output also being upper caseß
+    return dna_strand.replace("G", "c").replace("C", "g").replace("T", "a").replace("A", "u").upper()
