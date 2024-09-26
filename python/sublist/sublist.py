b980d8d63366a22c0bc40aa686bdc561a1578fb1
Gergely Imreh <gergely@imreh.net>
2024-09-26 10:07:41 +0800

    sublist initials submission

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -10,13 +10,34 @@ those constants (ex. the constants in the os, subprocess and re modules).
 You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 """
 
+from typing import Any
+
 # Possible sublist categories.
 # Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
+SUBLIST = 1
+SUPERLIST = 2
+EQUAL = 3
+UNEQUAL = 4
 
 
-def sublist(list_one, list_two):
-    pass
+# def _contains(list_one: list[Any], list_two: list[Any]) -> bool:
+
+
+def sublist(list_one: list[Any], list_two: list[Any]) -> int:
+    if list_one == list_two:
+        return EQUAL
+
+    len_one, len_two = len(list_one), len(list_two)
+    flipped = False
+    # Flip things around, so that the first string is always the longer
+    if len_two > len_one:
+        list_one, len_one, list_two, len_two = list_two, len_two, list_one, len_one
+        flipped = True
+
+    if len_two == 0:
+        return SUPERLIST if not flipped else SUBLIST
+    for start_index in range(0, len_one - len_two + 1):
+        if list_one[start_index : start_index + len_two] == list_two:
+            return SUPERLIST if not flipped else SUBLIST
+
+    return UNEQUAL
